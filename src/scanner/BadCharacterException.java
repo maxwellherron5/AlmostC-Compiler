13c5617be899674308a4985fd76c61d7aa03e9d5
Maxwell <maxwell@Maxwells-MacBook-Pro.local>
2020-01-24 16:55:43 -0600

    Created all necessary unit tests, added more documentation

--- src/scanner/BadCharacterException.java
+++ src/scanner/BadCharacterException.java
@@ -2,10 +2,24 @@ package scanner;
 
 import java.util.Objects;
 
+/**
+ * BadCharacterException.java
+ * @author Maxwell Herron
+ * This class is my custom exception that will be thrown when the
+ * scanner picks up lexeme input that is not matched with any of my
+ * specified tokentypes.
+ */
+
 public class BadCharacterException extends Exception
 {
+    // This string will hold the output error message
     private String errorMessage;
 
+    /**
+     * Creates the BadCharacterException object with an input error message
+     * associated with it.
+     * @param errorMessage
+     */
     public BadCharacterException(String errorMessage)
     {
         this.errorMessage = errorMessage;
--- src/scanner/ScannerTest.java
+++ src/scanner/ScannerTest.java
@@ -1,10 +1,16 @@
 package scanner;
 
 import org.junit.jupiter.api.Test;
-
 import java.io.StringReader;
-
 import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * ScannerTest.java
+ * @author Maxwell Herron
+ * This class contains seven unit tests that are used to test my scanner object.
+ * Five of these tests are testing a happy path and expect the output to match the expected.
+ * Two of them test sad paths, and expect the exception thrown to match the expected exception.
+ */
 
 class ScannerTest {
 
@@ -22,25 +28,45 @@ class ScannerTest {
     @Test
     void nextTokenHappy2()
     {
-
+        Token expected = new Token("myVariable", TokenType.IDENTIFIER);
+        Scanner scan = new Scanner(new StringReader("   myVariable    "));
+        try {
+            Token actual = new Token(scan.nextToken().lexeme, scan.nextToken().type);
+            assertEquals(expected, actual);
+        } catch (Exception e) { }
     }
 
     @Test
     void nextTokenHappy3()
     {
-
+        Token expected = new Token("/* this is a comment */", TokenType.COMMENT);
+        Scanner scan = new Scanner(new StringReader("/* this is a comment */"));
+        try {
+            Token actual = new Token(scan.nextToken().lexeme, scan.nextToken().type);
+            assertEquals(expected, actual);
+        } catch (Exception e) { }
     }
 
     @Test
     void nextTokenHappy4()
     {
-
+        Token expected = new Token("-2e23", TokenType.NUMBER);
+        Scanner scan = new Scanner(new StringReader("-2e23  "));
+        try {
+            Token actual = new Token(scan.nextToken().lexeme, scan.nextToken().type);
+            assertEquals(expected, actual);
+        } catch (Exception e) { }
     }
 
     @Test
     void nextTokenHappy5()
     {
-
+        Token expected = new Token("print", TokenType.KEYWORD);
+        Scanner scan = new Scanner(new StringReader("\n print"));
+        try {
+            Token actual = new Token(scan.nextToken().lexeme, scan.nextToken().type);
+            assertEquals(expected, actual);
+        } catch (Exception e) { }
     }
 
     @Test
@@ -58,6 +84,21 @@ class ScannerTest {
     @Test
     void nextTokenSad2()
     {
+        BadCharacterException expected = new BadCharacterException("Illegal char: '%' found.");
+        Scanner scan = new Scanner(new StringReader("int myVariable % print"));
+        Token result = null;
+        try {
+            result = scan.nextToken();
+        } catch (Exception actual) {
+            assertEquals(expected, actual);
+        }
+        while (result != null) {
+            try {
+                result = scan.nextToken();
+            } catch (Exception actual) {
+                assertEquals(expected, actual);
+            }
+        }
 
     }
 }
