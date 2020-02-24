3066ddc55938f4dc250bf6b35f892697d5e43f38
Maxwell Herron <Maxwell.herron@fjorgedigital.com>
2020-02-23 18:18:25 -0600

    Added javadoc comments for all functions in Recognizer.java

--- src/parser/Recognizer.java
+++ src/parser/Recognizer.java
@@ -12,7 +12,7 @@ import java.io.StringReader;
  * @author Maxwell Herron
  * This class builds all the grammatical rules necessary for a functioning
  * recursive descent recognizer.
- * Tests for this class can be found in /ParserTests/RecognizerTest.java
+ * Tests for this class can be found in RecognizerTest.java
  */
 public class Recognizer {
 
@@ -61,7 +61,7 @@ public class Recognizer {
     /////////////////////////
 
     /**
-     *
+     * Runs through the production for program.
      */
     public void program() {
         functionDeclarations();
@@ -73,7 +73,7 @@ public class Recognizer {
     }
 
     /**
-     *
+     * Runs through the production for identifierList
      */
     public void identifierList() {
         match(TokenType.IDENTIFIER);
@@ -84,7 +84,8 @@ public class Recognizer {
     }
 
     /**
-     *
+     * Runs through the production for declarations. Note that there is a lambda option
+     * if no type is present.
      */
     public void declarations() {
         if (isType()) {
@@ -98,7 +99,7 @@ public class Recognizer {
     }
 
     /**
-     *
+     * Determines which type keyword the token is. If there is no match, it will print an error.
      */
     public void type() {
         switch (lookahead.getType()) {
@@ -117,7 +118,8 @@ public class Recognizer {
     }
 
     /**
-     *
+     * Runs through the production for functionDeclarations. Note that there is a lambda option if no
+     * type is present.
      */
     public void functionDeclarations() {
         if (isType()) {
@@ -130,7 +132,7 @@ public class Recognizer {
     }
 
     /**
-     *
+     * Runs through the production for functionDeclaration.
      */
     public void functionDeclaration() {
         type();
@@ -139,7 +141,8 @@ public class Recognizer {
     }
 
     /**
-     *
+     * Runs through the production for functionDefinitions. Note that there is a lambda option if no
+     * type is present.
      */
     public void functionDefinitions() {
         if (isType()) {
@@ -151,7 +154,7 @@ public class Recognizer {
     }
 
     /**
-     *
+     * Runs through the production for functionDefinition.
      */
     public void functionDefinition() {
         type();
@@ -161,7 +164,7 @@ public class Recognizer {
     }
 
     /**
-     *
+     * Runs through the production for parameters.
      */
     public void parameters() {
         match(TokenType.LEFT_PARENTHESES);
@@ -170,7 +173,8 @@ public class Recognizer {
     }
 
     /**
-     *
+     * Runs through the production for parameterList. Note that there are two diverging options; if there is
+     * a comma present, it will call itself again.
      */
     public void parameterList() {
         type();
@@ -182,7 +186,7 @@ public class Recognizer {
     }
 
     /**
-     *
+     * Runs through the production for compoundStatement.
      */
     public void compoundStatement() {
         match(TokenType.LEFT_CURLY);
@@ -192,7 +196,8 @@ public class Recognizer {
     }
 
     /**
-     *
+     * Runs through the production for optionalStatements. Note that there is a lambda option if no
+     * statement is present.
      */
     public void optionalStatements() {
         if(isStatement()) {
@@ -203,7 +208,8 @@ public class Recognizer {
     }
 
     /**
-     *
+     * Runs through the production for statementList. Note that if a semicolon is present, it will recursively
+     * will recursively call itself until there is not one.
      */
     public void statementList() {
         statement();
@@ -214,7 +220,8 @@ public class Recognizer {
     }
 
     /**
-     *
+     * Runs through the production for statement. Uses the first lookahead tokenType value to
+     * determine which diverging rule to follow.
      */
     public void statement() {
         switch (lookahead.getType()) {
@@ -260,19 +267,21 @@ public class Recognizer {
     }
 
     /**
-     *
+     * Runs through the production for variable. If there is a left bracket present it will call
+     * expressionList and then match for a right bracket.
      */
     public void variable() {
         match(TokenType.IDENTIFIER);
-        if (lookahead.getType() == TokenType.LEFT_PARENTHESES) {
-            match(TokenType.LEFT_PARENTHESES);
+        if (lookahead.getType() == TokenType.LEFT_BRACKET) {
+            match(TokenType.LEFT_BRACKET);
             expressionList();
-            match(TokenType.RIGHT_PARENTHESES);
+            match(TokenType.RIGHT_BRACKET);
         }
     }
 
     /**
-     *
+     * Runs through the production for expressionList. If there is a comma, it will match the comma
+     * and call expression again.
      */
     public void expressionList() {
         expression();
@@ -283,7 +292,8 @@ public class Recognizer {
     }
 
     /**
-     *
+     * Runs through the production for simpleExpression. If it detects a sign, it will call sign,
+     * otherwise it will carry through with term and simplePart.
      */
     public void simpleExpression() {
         if (lookahead.getType() == TokenType.PLUS || lookahead.getType() == TokenType.MINUS) {
@@ -294,10 +304,11 @@ public class Recognizer {
     }
 
     /**
-     *
+     * Runs through the production for simplePart. Note that there is a lambda option if no addop
+     * is present.
      */
     public void simplePart() {
-        if (isAddop(lookahead)) {
+        if (isAddop()) {
             addop();
             term();
             simplePart();
@@ -307,7 +318,7 @@ public class Recognizer {
     }
 
     /**
-     *
+     * Runs through the production for term.
      */
     public void term() {
         factor();
@@ -315,10 +326,11 @@ public class Recognizer {
     }
 
     /**
-     *
+     * Runs through the production for termPart. Note that there is a lambda option if no
+     * mulop is present.
      */
     public void termPart() {
-        if (isMulop(lookahead)) {
+        if (isMulop()) {
             mulop();
             factor();
             termPart();
@@ -328,11 +340,12 @@ public class Recognizer {
     }
 
     /**
-     *
+     * Runs through the production for expression. Optionally calls relop and simpleExpression if
+     * a relop is detected after initially calling simpleExpression.
      */
     public void expression() {
         simpleExpression();
-        if (isRelop(lookahead)) {
+        if (isRelop()) {
             relop();
             simpleExpression();
         }
@@ -340,7 +353,8 @@ public class Recognizer {
 
 
     /**
-     *
+     * Runs through the production for factor. Uses the first lookahead TokenType to determine
+     * which diverging rule to follow.
      */
     public void factor() {
         switch (lookahead.getType()) {
@@ -377,7 +391,7 @@ public class Recognizer {
     }
 
     /**
-     *
+     * Determines what type of mulop the lookahead is, and prints an error if there is no match.
      */
     public void mulop() {
         switch (lookahead.getType()) {
@@ -399,7 +413,7 @@ public class Recognizer {
     }
 
     /**
-     *
+     * Determines what type of addop the lookahead is, and prints an error if there is no match.
      */
     public void addop() {
         switch (lookahead.getType()) {
@@ -418,7 +432,7 @@ public class Recognizer {
     }
 
     /**
-     *
+     * Determines what type of relop the lookahead is, and prints an error if there is no match.
      */
     public void relop() {
         switch (lookahead.getType()) {
@@ -445,6 +459,9 @@ public class Recognizer {
         }
     }
 
+    /**
+     * Determines what type of sign the lookahead is, and prints an error if there is no match.
+     */
     public void sign() {
         switch (lookahead.getType()) {
             case PLUS:
@@ -459,10 +476,10 @@ public class Recognizer {
     }
 
     /**
-     *
-     * @return
+     * Determines if the lookahead TokenType is a mulop.
+     * @return True if the lookahead TokenType is a mulop, false if not.
      */
-    public boolean isMulop(Token lookahead) {
+    public boolean isMulop() {
         boolean answer = false;
         if( lookahead.getType() == TokenType.MULTIPLY ||
                 lookahead.getType() == TokenType.DIVIDE ||
@@ -474,10 +491,10 @@ public class Recognizer {
     }
 
     /**
-     *
-     * @return
+     * Determines if the lookahead TokenType is an addop.
+     * @return True if the lookahead TokenType is an addop, false if not.
      */
-    public boolean isAddop(Token lookahead) {
+    public boolean isAddop() {
         boolean answer = false;
         if( lookahead.getType() == TokenType.PLUS ||
                 lookahead.getType() == TokenType.MINUS ||
@@ -488,10 +505,10 @@ public class Recognizer {
     }
 
     /**
-     *
-     * @return
+     * Determines if the lookahead TokenType is a relop.
+     * @return True if the lookahead TokenType is a relop, false if not.
      */
-    public boolean isRelop(Token lookahead) {
+    public boolean isRelop() {
         boolean answer = false;
         if( lookahead.getType() == TokenType.EQUAL ||
                 lookahead.getType() == TokenType.NOT_EQUAL ||
@@ -505,8 +522,8 @@ public class Recognizer {
     }
 
     /**
-     *
-     * @return
+     * Determines if the lookahead TokenType is a what is expected of a statement.
+     * @return True if the lookahead TokenType matches what is expected of a statement, false if not.
      */
     public boolean isStatement() {
         boolean answer = false;
@@ -520,21 +537,8 @@ public class Recognizer {
     }
 
     /**
-     *
-     * @return
-     */
-    public boolean isFactor() {
-        boolean answer = false;
-        if (lookahead.getType() == TokenType.IDENTIFIER || lookahead.getType() == TokenType.NUMBER ||
-            lookahead.getType() == TokenType.LEFT_PARENTHESES || lookahead.getType() == TokenType.NOT) {
-                answer = true;
-        }
-        return answer;
-    }
-
-    /**
-     *
-     * @return
+     * Determines if the lookahead TokenType is one of the type keywords.
+     * @return True if the lookahead TokenType is one of the type keywords, false if not.
      */
     public boolean isType() {
         boolean answer = false;
@@ -546,8 +550,8 @@ public class Recognizer {
     }
 
     /**
-     *
-     * @param message
+     * Prints an error caused by no available match.
+     * @param message the error message.
      */
     public void error(String message) {
         System.out.println( "Error " + message + " at line " +
@@ -556,8 +560,9 @@ public class Recognizer {
     }
 
     /**
-     *
-     * @param expected
+     * Attempts to match the lookahead TokenType with an expected TokenType. If no match is possible,
+     * it will call the error function to print out what was expected, and what the actual was.
+     * @param expected the expected TokenType
      */
     public void match(TokenType expected) {
         System.out.println("match( " + expected + ")");
